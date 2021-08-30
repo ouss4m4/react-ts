e5fa73779336a3b5cb9e1636c86b3d998ce776ff
oussama <bz.oussama@gmail.com>
2021-08-30 18:32:08 +0100

    renew key

--- src/api/api.tsx
+++ src/api/api.tsx
@@ -3,7 +3,9 @@ import { ajax } from "rxjs/ajax";
 import { catchError, map } from "rxjs/operators";
 import { IUser } from "../typings/typings";
 
-const APP_ID = "5fce783f75d63ad04da2c30b";
+// const APP_ID = "5fce783f75d63ad04da2c30b";
+const APP_ID = "612d15e406de53830c827a96";
+
 const BASE_URL = "https://dummyapi.io/data/v1";
 // https://dummyapi.io/data/v1/user?limit=10
 
